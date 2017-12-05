aa373e4b8889e621d47019787c5d4896aa19957b
Miquel Beltran <miquelbeltran@users.noreply.github.com>
2017-12-05 09:45:54 +0100

    Day 5 Part 2

--- src/Day5.kt
+++ src/Day5.kt
@@ -1,6 +1,6 @@
 import java.io.File
 
-class MazeSolver(input: List<String>) {
+class MazeSolver(input: List<String>, val isPart2: Boolean = false) {
     val lines = input.filter { it.isNotEmpty() }.map { it.toInt() }.toMutableList()
     var pos = 0
     var steps = 0
@@ -8,9 +8,16 @@ class MazeSolver(input: List<String>) {
     fun runStep() {
         steps++
         val offset = lines[pos]
-        lines[pos]++
+        val increment = if (part2Condition()) {
+            -1
+        } else {
+            1
+        }
+        lines[pos] += increment
         pos += offset
     }
+
+    private fun part2Condition() = isPart2 && lines[pos] >= 3
 
     fun isExit(): Boolean = pos >= lines.size
 }
@@ -18,7 +25,7 @@ class MazeSolver(input: List<String>) {
 fun main(args: Array<String>) {
     val lines = File("/Users/miquel/Downloads/input.txt")
             .readLines()
-    val solver = MazeSolver(lines)
+    val solver = MazeSolver(lines, true)
     while (!solver.isExit()) {
         solver.runStep()
     }
