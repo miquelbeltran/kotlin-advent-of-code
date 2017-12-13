d113d1689d6d681a9307063aff444f8b0b873a1f
Miquel Beltran <miquelbeltran@users.noreply.github.com>
2017-12-13 10:33:31 +0100

    Solved Day 13 part 2

---
+++ src/Day13.kt
@@ -0,0 +1,49 @@
+import java.io.File
+
+fun List<String>.linesToMap(): Map<Int, Int> {
+    return map { it.split(": ") }.associateBy({ it[0].toInt() }, { it[1].toInt() })
+}
+
+fun isDetected(delay: Int, layer: Int, map: Map<Int, Int>): Boolean {
+    val depth = map[layer]!!
+    return (layer + delay) % ((depth - 1)*2) == 0
+}
+
+fun solveDay13Part1(lines: List<String>): Int {
+    val map = lines.linesToMap()
+    var security = 0
+    map.forEach { layer, depth ->
+        if (isDetected(0, layer, map)) {
+            println("l: $layer d: $depth")
+            security += layer * depth
+        }
+    }
+    return security
+}
+
+fun solveDay13Part2(lines: List<String>): Int {
+    val map = lines.linesToMap()
+    var delay = 0
+    while (true) {
+        if (checkIfDetected(map, delay)) {
+            delay++
+        } else {
+            return delay
+        }
+    }
+}
+
+private fun checkIfDetected(map: Map<Int, Int>, delay: Int): Boolean {
+    for ((layer, depth) in map)
+        if (isDetected(delay, layer, map)) {
+            println("! at $layer with $delay")
+            return true
+        }
+    return false
+}
+
+fun main(args: Array<String>) {
+    val input = File("/Users/miquel/Downloads/input.txt").readLines()
+    println(solveDay13Part1(input))
+    println(solveDay13Part2(input))
+}
---
+++ src/TestDay13.kt
@@ -0,0 +1,30 @@
+import org.testng.annotations.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class TestDay13 {
+
+    @Test
+    fun `solve Day 13 Part 1`() {
+        val input = """0: 3
+1: 2
+4: 4
+6: 4"""
+        assertEquals(24, solveDay13Part1(input.lines()))
+    }
+
+    @Test
+    fun `position at picosecond`() {
+        val input = """0: 3
+1: 2
+4: 4
+6: 4"""
+        val map = input.lines().linesToMap()
+        assertTrue(isDetected(0, 0, map))
+        assertFalse(isDetected(1, 0, map))
+        assertTrue(isDetected(4, 0, map))
+    }
+
+}
+
