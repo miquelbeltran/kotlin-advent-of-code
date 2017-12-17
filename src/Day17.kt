aa895b14c64a53413475adaf787171606c00ee97
Miquel Beltran <miquelbeltran@users.noreply.github.com>
2017-12-17 11:31:45 +0100

    Day 17 Part 2

--- src/Day17.kt
+++ src/Day17.kt
@@ -6,11 +6,24 @@ fun solveDay17Part1(steps: Int): Int {
         pos = ((pos + steps) % (list.size)) + 1
         list.add(pos, it)
     }
-    println(list.subList(pos, pos+2))
     return list[pos + 1]
+}
+
+// Assume that Zero is always at pos 0
+fun solveDay17Part2(steps: Int): Int {
+    var pos = 0
+    var nextToZ = 0
+    (1..50_000_000).forEach {
+        pos = ((pos + steps) % (it)) + 1
+        if (pos == 1) {
+            nextToZ = it
+        }
+    }
+    return nextToZ
 }
 
 fun main(args: Array<String>) {
     val steps = 377
     println(solveDay17Part1(377))
+    println(solveDay17Part2(377))
 }
