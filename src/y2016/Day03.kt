1d896b8638ad326f04d7ad25f3d535c987b28065
Miquel Beltran <miquelbeltran@users.noreply.github.com>
2018-12-03 17:26:35 +0100

    2016.Day3

--- src/y2016/Day03.kt
+++ src/y2016/Day03.kt
@@ -15,7 +15,19 @@ object Day03 : Day {
     }
 
     override fun part2(input: List<String>): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var counter = 0
+        val parsed = input.map {
+            it.split(" ").filter { it.isNotEmpty() }.map { it.toInt() }
+        }
+        for (index in 0 until parsed.size step 3) {
+            val triangle0 = listOf(parsed[index][0], parsed[index + 1][0], parsed[index + 2][0])
+            val triangle1 = listOf(parsed[index][1], parsed[index + 1][1], parsed[index + 2][1])
+            val triangle2 = listOf(parsed[index][2], parsed[index + 1][2], parsed[index + 2][2])
+            counter += if (triangle0.none { it.toDouble() >= triangle0.sum().toDouble() / 2.toDouble() }) 1 else 0
+            counter += if (triangle1.none { it.toDouble() >= triangle1.sum().toDouble() / 2.toDouble() }) 1 else 0
+            counter += if (triangle2.none { it.toDouble() >= triangle2.sum().toDouble() / 2.toDouble() }) 1 else 0
+        }
+        return counter.toString()
     }
 
 }
