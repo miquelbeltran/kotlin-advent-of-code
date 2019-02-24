71c322bdd6dd6ae4d4f279e9b1e51ffcea68be4d
Miquel Beltran <miquelbeltran@users.noreply.github.com>
2019-02-24 21:14:55 +0100

    Solved day 9

--- src/y2018/Day09.kt
+++ src/y2018/Day09.kt
@@ -1,12 +1,13 @@
 package y2018
 
 import common.Day
+import java.util.*
 
 fun main() {
     Day09.solve()
 }
 
-object Day09: Day {
+object Day09 : Day {
     override fun part1(input: List<String>): String {
         val split = input.first().split(" ")
         val players = split.first().toInt()
@@ -20,7 +21,7 @@ object Day09: Day {
         var player = 0
         var scores = Array(players) { 0 }
 
-        for (marble in 1 .. last) {
+        for (marble in 1..last) {
             if (marble % 23 == 0) {
                 index -= 7
                 if (index < 0) {
@@ -52,7 +53,50 @@ object Day09: Day {
     }
 
     override fun part2(input: List<String>): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val split = input.first().split(" ")
+        val players = split.first().toInt()
+        val last = split[6].toInt() * 100
+        println("players $players")
+        println("last $last")
+
+        val list = mutableListOf<Int>()
+        list.add(0)
+        var index = 0
+        var player = 0
+        var scores = Array(players) { 0 }
+
+        for (marble in 1..last) {
+            if (marble % 10000 == 0) {
+                println(marble.toDouble() / last.toDouble())
+            }
+            if (marble % 23 == 0) {
+                index -= 7
+                if (index < 0) {
+                    index = list.size + index
+                }
+                scores[player] += list[index]
+                scores[player] += marble
+                list.removeAt(index)
+            } else {
+                if (list.size == 1) {
+                    index = 1
+                } else if (list.size == 2) {
+                    index = 1
+                } else {
+                    index = (index + 2)
+                    if (index > list.size) {
+                        index = 1
+                    }
+                }
+//                println(index)
+                list.add(index, marble)
+            }
+//            println(list)
+            player += 1
+            player %= players
+        }
+
+        return scores.max().toString()
     }
 
 }
